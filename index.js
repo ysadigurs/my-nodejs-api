1526244fbe349cb884ac9be0469550ac79723c21
ysadigurs <yona.sadigurs@gmail.com>
2024-09-12 11:31:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Vercel XXX!' });
+  res.json({ message: 'Hello from Vercel Great!' });
 });
 
 const PORT = process.env.PORT || 3000;
