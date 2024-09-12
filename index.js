9b37f3ba37b0e37d3d63179f67158cfc26dab638
ysadigurs <yona.sadigurs@gmail.com>
2024-09-12 16:03:57 +0300

    read json

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.get('/zmanim', (req, res) => {
   fetch('/data_zmanim_kavua.json') // Since it's in the public folder, it's accessible at the root
   .then(response => response.json())
   .then(data => {
-      const item =  data.find( record => (record["יום"] === req.query.name));
+      const item =  data.find( record => (record["יום"] === req.query.date));
       res.send(item);
   })
   .catch(error => {
-            console.error('Error fetching the leibovitz file', error);
+            console.error('Error fetching the zmanim file', error);
   });
 })
 
