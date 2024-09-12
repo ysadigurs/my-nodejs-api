88decc26c274d5664f85537d216211697add7f3e
ysadigurs <yona.sadigurs@gmail.com>
2024-09-12 16:00:27 +0300

    read json

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const app = express();
 const port = 3000;
 
 // Define routes and middleware
-app.get('/greet', (req, res) => {
-  const name = req.query.name || 'Guest';
-  res.send(`Hello, ${name}!`);
-});
+app.get('/zmanim', (req, res) => {
+
+  fetch('/data_zmanim_kavua.json') // Since it's in the public folder, it's accessible at the root
+  .then(response => response.json())
+  .then(data => {
+      const item =  data.find( record => (record["יום"] === req.query.name));
+      res.send(item);
+  })
+  .catch(error => {
+            console.error('Error fetching the leibovitz file', error);
+  });
+})
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+
