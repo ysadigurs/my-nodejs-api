52f29e499d1da3bf069ccd6e89ddbc47308254d5
ysadigurs <yona.sadigurs@gmail.com>
2024-09-12 14:55:37 +0300

    support input params

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
+// index.js
 const express = require('express');
 const app = express();
+const port = 3000;
 
-
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Vercel Great Again!' });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-// Middleware to parse JSON request body
-// app.use(express.json());
-
-// Route with query parameters
 app.get('/greet', (req, res) => {
   const name = req.query.name || 'Guest';
   res.send(`Hello, ${name}!`);
 });
 
-// Route with route parameters
-app.get('/user/:id', (req, res) => {
-  const userId = req.params.id;
-  res.send(`User ID: ${userId}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
-
-// POST request with JSON body
-app.post('/data', (req, res) => {
-  const data = req.body;
-  res.send(`Received data: ${JSON.stringify(data)}`);
-});
-
-
